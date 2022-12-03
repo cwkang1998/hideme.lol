7dbfd7471276d17f31f719de75a7de3a44c68b8b
Carlos Ramos <88168831+jrcarlos2000@users.noreply.github.com>
2022-12-03 11:09:31 +0800

    feat : push protocol integration (#5)

--- web/src/views/Dashboard.tsx
+++ web/src/views/Dashboard.tsx
@@ -1,6 +1,8 @@
+import SendPushNotification from "../utils/PushProtocol"
 
 export const Dashboard = () => {
+
     return (
-        <>Hello World</>
+        <div><button onClick={() => SendPushNotification("0x4bdB8234AD81F26985d257F36a2d2d8c30365546")}>Hola</button></div>
     )
 }
